1ebf9eff4de20e394255a5b4110cf4dd9eb3fa1e
jasoncym <53342853+jasoncym@users.noreply.github.com>
2024-10-23 19:03:59 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
-import type { FC } from 'react'
-import React from 'react'
+import type { FC } from 'react';
+import React from 'react';
+import Head from 'next/head'; // 导入 Head 组件
 
-import type { IMainProps } from '@/app/components'
-import Main from '@/app/components'
+import type { IMainProps } from '@/app/components';
+import Main from '@/app/components';
 
-const App: FC<IMainProps> = ({
-  params,
-}: any) => {
+const App: FC<IMainProps> = ({ params }: any) => {
   return (
-    <Main params={params} />
-  )
-}
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/public/favicon-48x48.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/public/web-app-manifest-192x192.png" />
+        <link rel="manifest" href="/public/site.webmanifest" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      </Head>
+      <Main params={params} />
+    </>
+  );
+};
 
-export default React.memo(App)
+export default React.memo(App);
